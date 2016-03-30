e1c52ea4e26043b1e17d083d11856ba60d0fe39f
BubbleDJ10 <bubbledj10@gmail.com>
2016-03-30 07:29:25 -0700

    Added Basic Scripts

---
+++ ButtonClick1.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonClick1 : MonoBehaviour {
+
+	public void MouseButtonClick(){
+		Debug.Log ("You clicked the button");
+	}
+}
---
+++ StrokesSaver.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StrokesSaver : MonoBehaviour {
+
+	public Text strokes_to_display;
+
+	//public UISystem strokes_manager;
+
+	private int finalstrokes2;
+
+	public int finalStrokes;
+
+	private int check = 0;
+
+	private int derp = 7;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		/*if(check != 1){
+
+			finalstrokes2 = UISystem.strokes;
+
+			Debug.Log (finalstrokes2);
+
+			strokes_to_display.text = finalstrokes2.ToString();
+			++check;
+		}*/
+
+		finalstrokes2 = UISystem.strokes;
+
+		Debug.Log (finalstrokes2);
+
+		strokes_to_display.text = finalstrokes2.ToString();
+
+	}
+
+	public void transferStrokes (){
+
+		strokes_to_display.text = finalstrokes2.ToString();
+	}
+
+	/*(public void stupid(){
+
+		finalstrokes2 = strokes_manager.strokes2;
+
+		strokes_to_display.text = finalstrokes2.ToString ();
+		Debug.Log ("TEST");
+	}*/
+		
+}
